3ac580d2f30d9e37b4b27750fe3014ae9c2b27bb
Ricky Mormor <ricky@appsnmobilesolutions.com>
2023-02-21 13:23:51 +0000

    ✅ FEAT: destroy previous scaffold and implement devise for user

--- app/models/contact.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Contact < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :phone, presence: true
-end
